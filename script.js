8b1884c65077d517221c196a6b5913c916294a83
LJDJGC <singsongsmusic@gmail.com>
2025-08-21 21:37:31 +0900

    Finish Rock-Paper-Scissors: playRound inside playGame, score tracking and winner output

--- script.js
+++ script.js
@@ -15,5 +15,53 @@ function getHumanChoice() {
 
 console.log(getHumanChoice());
 
-let humanScore = 0;
-let computerScore = 0;
+
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+
+    function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+
+    if (humanChoice === "rock" && computerChoice === "paper") {
+          console.log("You lose! Paper beats Rock");
+          computerScore++;
+      } else if (humanChoice === "paper" && computerChoice === "scissors") {
+          console.log("You lose! Scissors beats Paper");
+          computerScore++;
+      } else if (humanChoice === "scissors" && computerChoice === "rock") {
+          console.log("You lose! Rock beats Scissors");
+          computerScore++;
+      } else if (humanChoice === "rock" && computerChoice === "scissors") {
+          console.log("You win! Rock beats Scissors");
+          humanScore++;
+      } else if (humanChoice === "paper" && computerChoice === "rock") {
+          console.log("You win! Paper beats Rock");
+          humanScore++;
+      } else if (humanChoice === "scissors" && computerChoice === "paper") {
+          console.log("You win! Scissors beats Paper");
+          humanScore++;
+      } else if (humanChoice === computerChoice) {
+          console.log("Draw!");
+      }
+    }
+
+    for (let i = 0; i < 5; i++) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+
+        playRound(humanSelection, computerSelection);
+    }
+
+    console.log("Final score: Human " + humanScore + " - Computer " + computerScore);
+
+    if (humanScore > computerScore) {
+        console.log("You win the game!");
+    } else if (humanScore < computerScore) {
+        console.log("Computer wins the game!");
+    } else if (humanScore === computerScore) {
+        console.log("Draw!");
+    }
+}
+
+playGame();
